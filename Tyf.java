bc9bbb7f555cf9c95c3eac8a2708331640aeeaa9
Bekhruz <bekhruzabdubsitov7s@gmail.comb>
2023-11-07 16:16:04 +0500

    nm

--- Tyf.java
+++ Tyf.java
@@ -1,5 +1,19 @@
 public class Tyf {
     public static void main(String[] args) {
-        System.out.println("Akbar lox");
+        int [] a = {1, -3,  2, 0, -1, -1, 2, 1, 0,0 };
+        int nol = 0;
+        int musbat = 0;
+        int manfi = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0 ) {
+                musbat++;
+            }
+            if (a[i] == 0 ) {
+                nol++;
+            }
+            if (a[i] < 0 ) {
+                manfi++;
+            }
+        }
     }
 }
